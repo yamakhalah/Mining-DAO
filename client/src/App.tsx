b86c52d0f82b2e60d42d07b6996146716982297c
yamakhalah <divitodylan@gmail.com>
2023-04-06 12:28:15 +0200

    Wagmi wallet connector

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,49 @@
 import React from 'react'
-import { EthProvider } from './contexts/EthContext'
 import { BrowserRouter } from 'react-router-dom'
 import { getApp } from './utils/helpers'
 import theme from './style/theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
+import '@rainbow-me/rainbowkit/styles.css'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { arbitrumGoerli } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [arbitrumGoerli],
+  [
+    alchemyProvider({ apiKey: 'YVEO9LH2Wsju9Wn8pDSKJUs4VMA2kLTo' }),
+    publicProvider()
+  ]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'Mining DAO App',
+  chains
+})
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+  webSocketProvider
+})
 
 const App = (): JSX.Element => {
   const AppWithDomain = getApp()
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <EthProvider>
-        <div id="App">
-          <BrowserRouter>
-            <AppWithDomain />
-          </BrowserRouter>
-        </div>
-      </EthProvider>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <CssBaseline />
+            <div id="App">
+              <BrowserRouter>
+                <AppWithDomain />
+              </BrowserRouter>
+            </div>
+        </RainbowKitProvider>
+      </WagmiConfig>
     </ThemeProvider>
   )
 }
